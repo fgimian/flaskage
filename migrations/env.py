d56c70f53b80cfd6cd66a278c88f7c839d310b45
Fotis Gimian <fgimiansoftware@gmail.com>
2014-03-27 18:41:58 +1100

    Added the compare_type property to migrations to ensure that changes to fields are detected

--- migrations/env.py
+++ migrations/env.py
@@ -39,7 +39,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+    context.configure(url=url, compare_type=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -58,7 +58,8 @@ def run_migrations_online():
 
     connection = engine.connect()
     context.configure(
-        connection=connection, target_metadata=target_metadata)
+        connection=connection, target_metadata=target_metadata,
+        compare_type=True)
 
     try:
         with context.begin_transaction():
