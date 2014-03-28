8d81bb27fde5b84c27e9147f7828876d95c1b6a6
Fotis Gimian <fgimiansoftware@gmail.com>
2014-03-28 23:32:14 +1100

    The shell provided now uses ipython and includes models in its context

--- manage.py
+++ manage.py
@@ -13,7 +13,6 @@
 """
 import os
 import fnmatch
-import sys
 
 from flask.ext.failsafe import failsafe
 from flask.ext.script import Manager
@@ -22,19 +21,20 @@ from flask.ext.migrate import MigrateCommand
 from flask.ext.assets import ManageAssets
 
 from config import AVAILABLE_CONFIGS, DEFAULT_CONFIG
-from application import create_app, db
-
-# Import all models so that they are visible to Flask-Migrate.  We also
-# issue a noqa command to avoid flake8's unused import warning.
-import application.models  # noqa
+from application import create_app, db, models
 
 manager = Manager(failsafe(create_app), with_default_commands=False)
+
+
+def _make_context():
+    return dict(app=manager.app, db=db, models=models)
+
 manager.add_option('-c', '--config', dest='config',
                    choices=AVAILABLE_CONFIGS.keys(), default=DEFAULT_CONFIG)
 manager.add_command('clean', Clean())
 manager.add_command('runserver', Server())
 manager.add_command(
-    'shell', Shell(make_context=lambda: {'app': manager.app, 'db': db}))
+    'shell', Shell(make_context=_make_context))
 manager.add_command('urls', ShowUrls())
 manager.add_command('db', MigrateCommand)
 manager.add_command("assets", ManageAssets())
