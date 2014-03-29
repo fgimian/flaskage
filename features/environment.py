9ba1b9337d4856a9eef84a371ce06d905e0289b3
Fotis Gimian <fgimiansoftware@gmail.com>
2014-03-29 23:41:10 +1100

    Improved behave example BDD tests to be more Pythonic and pass flake8 tests

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from application import create_app, db
 
+
 def before_feature(context, feature):
     context.app = create_app('testing')
     context.client = context.app.test_client()
     context.ctx = context.app.test_request_context()
     context.ctx.push()
     db.create_all()
+
 
 def after_feature(context, feature):
     db.session.remove()
--- features/steps/home_page_steps.py
+++ features/steps/home_page_steps.py
@@ -1,17 +1,17 @@
 from behave import given, when, then
 
 
-@given('we have a Flaskage-based application setup')  # noqa
-def step_impl(context):
+@given('we have a Flaskage-based application setup')
+def given_we_have_application(context):
     assert context.client and context.ctx
 
 
-@when('I load the home page at "{url_path}"')  # noqa
-def step_impl(context, url_path):
+@when('I load the home page at "{url_path}"')
+def when_load_home_page(context, url_path):
     context.response = context.client.get(url_path)
     assert context.response
 
 
-@then('ensure the word "{expected_word}" is displayed')  # noqa
-def step_impl(context, expected_word):
+@then('ensure the word "{expected_word}" is displayed')
+def then_ensure_word_displayed(context, expected_word):
     assert expected_word.encode('u8') in context.response.data
