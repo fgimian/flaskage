aedc8470eb34086a90d939ee7f95e5218a57e649
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-06 23:20:36 +1000

    Added example code for 403 and 500 errorhandlers

--- application/__init__.py
+++ application/__init__.py
@@ -55,9 +55,17 @@ def create_app(config):
     #     pass
 
     # Setup app error handlers if necessary
+    # @app.errorhandler(403)
+    # def forbidden(error):
+    #     return render_template("403.html"), 403
+
     # @app.errorhandler(404)
     # def not_found(error):
     #     return render_template("404.html"), 404
+
+    # @app.errorhandler(500)
+    # def internal_server_error(error):
+    #     return render_template("500.html"), 500
 
     # Import and register Blueprints
     from .views import flaskage
