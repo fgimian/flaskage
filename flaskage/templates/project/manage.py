ff29e2ea7e1989d0e151296ec7d10e9f7d8e8ca6
Fotis Gimian <fgimiansoftware@gmail.com>
2014-05-03 00:36:04 +1000

    Improved context maker so that all models and factories will be available in debug shell

--- flaskage/templates/project/manage.py
+++ flaskage/templates/project/manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from inspect import getmembers, isclass
+
 from flask.ext.failsafe import failsafe
 from flask.ext.script import Manager
 from flask.ext.script.commands import Clean, Server, Shell, ShowUrls
@@ -14,7 +16,17 @@ manager = Manager(failsafe(create_app), with_default_commands=False)
 
 
 def _make_context():
-    return dict(app=manager.app, db=db, models=models, factories=factories)
+    context = dict(app=manager.app, db=db)
+    # Add models to the context
+    context.update(getmembers(
+        models, lambda m: isclass(m) and issubclass(m, db.Model)
+    ))
+    # Add factories to the context
+    context.update(getmembers(
+        factories,
+        lambda m: isclass(m) and issubclass(m, SQLAlchemyModelFactory)
+    ))
+    return context
 
 manager.add_option('-c', '--config', dest='config',
                    choices=list(AVAILABLE_CONFIGS), default=DEFAULT_CONFIG)
